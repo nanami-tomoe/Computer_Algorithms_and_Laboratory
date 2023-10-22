d5bc6814c10af6a7ae9daf890cb26e8f92e74d8f
nanami-tomoe <npnp123npnp123@gmail.com>
2023-10-22 23:54:59 +0900

    유클리드 호제법 문자열 암호화하기

--- ex4-1.cpp
+++ ex4-1.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string> // 문자열 처리시 필요한 라이브러리
+#define MESSAGE_LENGTH 10
 using namespace std;
 
 int main() {
     string myMessage = "Hello, please love woongsup";
     char encryption[MESSAGE_LENGTH];
 
-    for (i = 0; i < myMessage.length(); i++) {
-        c = (usigned int) mtMessage.at(i);
+    // 문자열의 각 문자에 대해 key의 xor 연산후 문자 배열에 저장
+
+    for (int i = 0; i < myMessage.length(); i++) {
+        int c = (usigned int) mtMessage.at(i);
         c = c ^ key;
         encryption[i] = (char) c;
     }
